1d90a20b38f1cd9be1c3c2f19b004886d277c5da
João Pedro Rodrigues <joopedroe936@gmail.com>
2019-02-15 20:22:21 -0300

    inicial

--- LPOOpython.py
+++ LPOOpython.py
@@ -1,28 +1,31 @@
-class Pessoa():
+class Pessoa:
     def __init__(self,nome,data_nascimento):
         self.nome=nome
         self.data_nascimento=data_nascimento
-    def mudarNome(self,nome):
-        self.nome=nome
+    def __str__(self):
+        return "Pessoa:"+self.nome+"data: "+self.data_nascimento
         
-class Projeto():
+class Projeto:
     def __init__(self,nome,data_inicio,data_fim):
         self.nome=nome
         self.data_inicio=data_inicio
         self.data_fim=data_fim
         
 
-class Atividade():
-    def __init__(self,nome,prioridade,nomePessoa,nasci,nomePro,inicio,fim):
+class Atividade:
+    def __init__(self,nome,prioridade,pessoa,projeto):
         self.nome=nome
         self.prioridade=prioridade
-        self.pessoa=Pessoa(nomePessoa,nasci)
-        self.projeto=Projeto(nomePro,inicio,fim)
+        self.pessoa=pessoa
+        self.projeto=projeto
+    def __str__(self):
+        return "Atividade : nome :"+ self.nome +"   Prioridade: "+str(self.prioridade)
 
-p=Atividade("Troca de gerência","Máxima","João","03/09/98","Mudança de cargo","2018","2019")
-print(p.pessoa.nome,'\n')
-p.pessoa.mudarNome("Pedro")
-print(p.pessoa.nome)
+p=Pessoa("João","03-09-98")
+pro=Projeto("Projeto 1","03-02-2019","20-10-2019")
+ati=Atividade("Atividade 1",1,p,pro)
+print(ati.pessoa.nome,'\n')
+print(ati)
 
 
 
